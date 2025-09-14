0f693e9c6deae19072321d95ddb425fdee19f6e3
VictorHBPortela <victor.portela@duotalk.com.br>
2025-09-14 18:22:34 -0300

    Implement socket authentication and message handling; add validation and client management

--- server.ts
+++ server.ts
@@ -3,35 +3,51 @@ import fs from "fs";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { fetchOrCreateFile } from "./fetchOrCreateFile.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { validateToken } from "./helpers/validateToken.js";
 
+dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const path = "clients.json";
 
 export const startServer = () => {
-  io.on("connection", (socket) => {
+  io.use((socket, next) => {
+    validateToken(socket, next);
+  }).on("connection", (socket) => {
     console.log("A user connected, ID:", socket.id);
-    let clients = fetchOrCreateFile(path);
-
-    if (!clients.includes(socket.id)) {
+    try {
+      let clients = fetchOrCreateFile(path);
       clients.push(socket.id);
       fs.writeFileSync(path, JSON.stringify(clients, null, 2));
-      console.log("Client added. Current clients:", clients.length);
-    } else {
-      console.log("Client already exists in list");
+    } catch (error) {
+      console.log("Error updating client list", error);
     }
 
     socket.on("disconnect", (reason) => {
       console.log("User disconnected, ID:", socket.id, "Reason:", reason);
-      let clients = fetchOrCreateFile(path);
-      clients = clients.filter((c) => c !== socket.id);
-      fs.writeFileSync(path, JSON.stringify(clients, null, 2));
-      console.log("Updated clients file. Current clients:", clients.length);
+      try {
+        let clients = fetchOrCreateFile(path);
+        clients = clients.filter((c) => c !== socket.id);
+        fs.writeFileSync(path, JSON.stringify(clients, null, 2));
+      } catch (error) {
+        console.log("Error removing client from list: ", error);
+      }
+    });
+
+    socket.on("user message", (msg) => {
+      socket.broadcast.emit("user message", msg);
     });
   });
 
-  server.listen(3000, () => {
-    console.log("Server running on localhost:3000");
-  });
+  server
+    .listen(3000, () => {
+      console.log("Server running on localhost:3000");
+    })
+    .on("error", (error) => {
+      console.error("Server failed to start:", error);
+      process.exit(1);
+    });
 };
